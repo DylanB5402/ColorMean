b45886c70ef5285dde5b4725e6e3e82e878cfbc5
Dylan <d.barva5402@gmail.com>
2020-07-27 20:18:20 -0700

    write wrapper class

--- colormean/image.py
+++ colormean/image.py
@@ -3,7 +3,9 @@ import cv2
 class Image:
 
     def __init__(self, file_name : str):
-        self.img_array = cv2.imread(file_name)
+        self.img = cv2.imread(file_name)
+        self.row_count = self.img.shape[0]
+        self.column_count = self.img.shape[1]
 
     def set_bgr(self, row : int, col : int, b : int, g : int, r : int):
         self.set_blue(row, col, b)
@@ -11,13 +13,13 @@ class Image:
         self.set_red(row, col, r)
 
     def set_blue(self, row : int, col : int, b : int):
-        self.img_array.itemset((row, col, 0), b)
+        self.img.itemset((row, col, 0), b)
 
     def set_green(self, row : int, col : int, g : int):
-        self.img_array.itemset((row, col, 1), g)
+        self.img.itemset((row, col, 1), g)
 
     def set_red(self, row : int, col : int, r : int):
-        self.img_array.itemset((row, col, 2), r)
+        self.img.itemset((row, col, 2), r)
 
     def save(self, new_file_name : str):
-        cv2.imwrite(new_file_name, self.img_array)
+        cv2.imwrite(new_file_name, self.img)
--- colormean/main.py
+++ colormean/main.py
@@ -1,15 +1,15 @@
 import cv2
+from image import Image
 
 
 def make_mean_pic(rows : int, columns : int, img_file_name : str):
-    img = cv2.imread(img_file_name)
-    print(type(img))
+    pic = Image(img_file_name)
+    # for row in range(pic.img.shape[0]):
+    #     for column in range(pic.img.shape[1]):
+    #         pic.set_bgr(row, column, 0, 0, 0)
+    for x in range(1, rows):
+        print(x)
 
-    # for row in range(img.shape[0]):
-    #     for column in range(img.shape[1]):
-    #         img[row, column] = [0, 0, 0]
-
-    # cv2.imwrite("quadrants-new.jpg", img)
 
 
 
@@ -17,4 +17,4 @@ def make_mean_pic(rows : int, columns : int, img_file_name : str):
 def format_color(bgr : list) -> str:
     return 'blue ' + str(bgr[0]) + " green " + str(bgr[1]) + ' red ' + str(bgr[2])
 
-make_mean_pic(1, 1, "img/quadrants.jpg")
+make_mean_pic(2, 1, "img/quadrants.jpg")
