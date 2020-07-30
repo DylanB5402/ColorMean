53ab05dd468b8b154d7f43145e878b897b917bd9
Dylan <d.barva5402@gmail.com>
2020-07-29 19:56:55 -0700

    delete some stuff

--- colormean/main.py
+++ colormean/main.py
@@ -1,4 +1,3 @@
-import cv2
 from image import Image
 
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_core/py_basic_ops/py_basic_ops.html
@@ -8,7 +7,6 @@ def format_color(bgr : list) -> str:
     return 'blue ' + str(bgr[0]) + " green " + str(bgr[1]) + ' red ' + str(bgr[2])
 
 
-quadrants = Image("img/vani.jpg")
+quadrants = Image("img/pua.png")
 quadrants.gen_mean_pic(50, 50)
 quadrants.save("pic.jpg")
-# quadrants.section_array[0].calc_average_color()
